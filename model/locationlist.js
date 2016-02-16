b9c8854edfee618b7e06bd2890b1b535f108ad05
Chauhan Akash(チョウハン　アカーシュ) <ch.m.aakash@gmail.com>
2016-02-16 17:26:40 +0000

    list and detail draft

---
+++ model/locationlist.js
@@ -0,0 +1,53 @@
+var placeList = [ 
+
+  { 
+
+    "id": 1, 
+
+    "name": "Tokyo apartment with view", 
+
+    "address": "3­4­5 Shibuya", 
+
+    "owner": { 
+
+      "id": 1, 
+
+      "name": "Aoki Tomoya" 
+
+    }, 
+
+    "coordinates": { 
+
+      "latitude": 35.1, 
+
+      "longitude": 139.5 
+
+    } 
+
+  }, 
+  { 
+
+    "id": 2, 
+
+    "name": "Tokyo apartment without view", 
+
+    "address": "4­5­6 Shinjuku", 
+
+    "owner": { 
+
+      "id": 2, 
+
+      "name": "Tomoya Aoki" 
+
+    }, 
+
+    "coordinates": { 
+
+      "latitude": 35.2, 
+
+      "longitude": 140.5 
+
+    } 
+
+  }
+];
